ec786ef38f4d9aa00046b96e9350ca23af0af838
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 19:09:30 +0100

    proje ile ilk ders

---
+++ sayac/sayac.js
@@ -0,0 +1,17 @@
+const arttir = document.getElementById("arttir");
+const azalt = document.getElementById("azalt");
+const sonuc = document.getElementById("sonuc");
+
+let sayac = 0;
+
+function sayacArttir(){
+    sayac++;
+
+    sonuc.innerHTML = sayac;
+}
+
+function sayacAzalt(){
+    sayac--;
+
+    sonuc.innerHTML = sayac;
+}
